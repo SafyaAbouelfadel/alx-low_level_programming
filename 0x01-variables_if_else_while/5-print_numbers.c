ead45ce05fc6af201893780fc402e39fa9452d31
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-21 16:36:11 -0700

    A C progam that print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a C program that print numbers with printf function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	 int number;
+
+	number = 0;
+
+	while (number <= 9)
+	{
+	printf(number);
+	number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
