7649b10b54e773334330647de70b7bcc7418f2e2
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-21 13:15:13 -0700

    A C progam that print alphabete with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a C program that print alphabets with putchar function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	while (ch > 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+}
