19a390c324ad4ae84c4af2d6f03c38c45e0ce8d0
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-21 16:05:40 -0700

    A C progam that print alphabete escept e and q with putchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,29 +4,28 @@
  * main - Entry point.
  *
  * Description: a C program that print alphabets with putchar function
- *	in lower case and upper case
+ *	wile skipping 'e' and 'q'.
  *
  * Return: 0 (Success)
 */
 
 int main(void)
 {
-	char chlow;
-	char chup;
+	char ch;
 
-	chlow = 'a';
-	chup = 'A';
+	ch = 'a';
 
-	while (chlow <= 'z')
+	while (ch <= 'z')
 	{
-		putchar(chlow);
-		chlow++;
-	}
-
-	while (chup <= 'Z')
-	{
-		putchar(chup);
-		chup++;
+		if (ch != 'e' || ch != 'q')
+		{
+			putchar(ch);
+			ch++,
+		}
+		else
+		{
+			ch++;
+		}
 	}
 	putchar('\n');
 
