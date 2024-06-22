e07f118906f474af189b6d45060705d765bfd158
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-21 20:32:11 -0700

    A C progam that print all possible different combinations of two digits with putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a C program that print numbers with printf function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	num1 = 48;
+	num2 = 48;
+	while (num1 <= 57)
+	{
+		putchar(num1);
+		num1++;
+	while (num2 <= 57)
+	{
+		if (num1 >= num2)
+		{
+			num2++;
+		}
+		else
+		{
+			putchar(num2);
+			num2++
+		}
+	}
+	if (num1 >= 56 && num2 >= 57)
+	{
+		return (0);
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
