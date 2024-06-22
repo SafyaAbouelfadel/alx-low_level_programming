2817c4f7f85b18e60d53b79f91b07e8390ecdcb7
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-21 19:05:44 -0700

    A C progam that print numbers with a comma with putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a C program that print numbers with printf function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number);
+		putchar(',');
+		putchar(' ');
+		number++;
+
+	}
+	putchar('\n');
+
+	return (0);
+}
