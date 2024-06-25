19baca635c5a260c01b7b78b0c8442c54b864785
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-25 16:47:18 -0700

    putchar

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
-		sum = fibl + fib2;
+		sum = fib1 + fib2;
 		printf("%lu", sum);
 		fib1 = fib2;
 		fib2 = sum;
@@ -27,3 +27,4 @@ int main(void)
 	}
 
 	return (0);
+}
