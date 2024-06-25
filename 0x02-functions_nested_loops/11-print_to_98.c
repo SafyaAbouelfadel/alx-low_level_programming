98f8c0242955980a151dc4bb826e7376ae6d961a
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-25 15:32:13 -0700

    putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,10 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-	for (count = 0; count > 98; count--)
+	for (count = n; count > 98; count--)
 		printf("%d, ", count);
 	else
-	for (count = 0; count < 98; count++)
+	for (count = n; count < 98; count++)
 		printf("%d, ", count);
 	printf("98\n");
 }
