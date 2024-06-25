1eaeed5deade6328d62aed3910c61852274d81b4
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-25 12:02:23 -0700

    putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 	else if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else
 	{
-		_purchar('0');
+		_purchar(48);
 		return (0);
 	}
 }
