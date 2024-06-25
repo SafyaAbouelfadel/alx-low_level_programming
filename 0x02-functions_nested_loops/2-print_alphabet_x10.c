9effa4ba55819e1994ef999a58b6c3e680b02b18
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-25 11:21:02 -0700

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- * print_alphabet X 10  - utilizes on the _putchar function to print
+ * print_alphabet_x10  - utilizes on the _putchar function to print
  *			the alphabet a - z 10 times
  *
- * Description: a C program that utilizes on the _putchar to print 10 times
- *			the alphabete
+ * Description: 'a C program that utilizes on the _putchar to print 10 times
+ *			the alphabete'
 */
 
 
