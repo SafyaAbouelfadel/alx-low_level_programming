5c782348874a5e1a97a0d84f442ec502bc874cd7
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-25 17:34:41 -0700

    putchar

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 98 Fibonacci numbers
+ * Return: 0 (Success)
+ */
+
 void print_fibonacci_98(void);
 
 int main(void)
 {
-    print_fibonacci_98();
-    return 0;
+	print_fibonacci_98();
+	return (0);
 }
+
+/**
+ * print_fibonacci_98 - Prints the first 98 Fibonacci numbers
+ *
+ * Description: Prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, followed by a new line. Numbers are separated by a comma
+ * and a space.
+ */
 
 void print_fibonacci_98(void)
 {
-    unsigned long num1_high = 0, num1_low = 1;
-    unsigned long num2_high = 0, num2_low = 2;
-    unsigned long high, low;
-    int count;
+	unsigned long num1_high = 0, num1_low = 1;
+	unsigned long num2_high = 0, num2_low = 2;
+	unsigned long high, low;
+	int count;
 
-    printf("%lu, %lu", num1_low, num2_low);
-    for (count = 2; count < 98; count++)
-    {
-        high = num1_high + num2_high;
-        low = num1_low + num2_low;
-        if (low >= 10000000000)
-        {
-            high++;
-            low -= 10000000000;
-        }
-        
-        printf(", ");
-        if (high > 0)
-            printf("%lu%010lu", high, low);
-        else
-            printf("%lu", low);
+	printf("%lu, %lu", num1_low, num2_low);
+	for (count = 2; count < 98; count++)
+	{
+	high = num1_high + num2_high;
+	low = num1_low + num2_low;
+	if (low >= 10000000000)
+	{
+		high++;
+		low -= 10000000000;
+	}
 
-        num1_high = num2_high;
-        num1_low = num2_low;
-        num2_high = high;
-        num2_low = low;
-    }
-    printf("\n");
+	printf(", ");
+	if (high > 0)
+		printf("%lu%010lu", high, low);
+	else
+		printf("%lu", low);
+
+	num1_high = num2_high;
+	num1_low = num2_low;
+	num2_high = high;
+	num2_low = low;
+	}
+	printf("\n");
 }
