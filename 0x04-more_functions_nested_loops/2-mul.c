57fa30f2c32babcd73f3fb1fb2b8e9f37269a067
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 12:42:46 -0700

    a function that return the multi of two digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two input digit
+ *
+ * @a: first input
+ * @b: seconde input
+ *
+ * Return: return results
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
