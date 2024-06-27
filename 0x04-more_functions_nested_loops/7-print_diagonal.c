8bf3db8c103435b3dc88c58830ce7528abf03ef8
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 16:19:13 -0700

    a function that draw diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: takes numiric input for function
+ *
+ * Return: 0 (Success)
+*/
+
+void print_diagonal(int n)
+{
+	int num, lines, phyramid;
+
+	phyramid = 1;
+
+	if (n > 0)
+	{
+		for (num = 0; num < n; num++)
+		{
+			for (lines = 0; lines <= phyramid; lines++)
+			{
+				_putchar(' ');
+			}
+		phyramid++;
+		_putchar(92);
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
