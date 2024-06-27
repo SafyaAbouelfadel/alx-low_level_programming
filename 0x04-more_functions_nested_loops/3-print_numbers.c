a1ba96a8c06513729177aaac71b4621544fd5c6b
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 12:52:57 -0700

    a function that return the multi of two digit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
  *
  * @c: take numiric type input for function
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 void print_numbers(void);
@@ -14,7 +14,7 @@ void print_numbers(void);
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + 48);
+		_putchar(i + '48');
 	_putchar('\n');
 
 	return (0);
