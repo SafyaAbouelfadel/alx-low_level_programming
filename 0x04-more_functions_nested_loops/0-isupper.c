f2eda5e8edcb965bc2fb04e94389603e8dc5b028
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 11:46:40 -0700

    check if c is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: checks for uppercase character
+ *
+ * @c: takes characheter type imput for function
+ *
+ * Return: 1 if @c is uppercase else 0
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	return (1);
+	else
+	return (0);
+}
