d3068c68b446846ac91766af3b9535b749b2cdbd
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 18:33:27 -0700

    a function that draw a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square,
+ *		followed by a new line.
+ *@size: takes input for the size of
+ *		the square
+ * 
+ * Return: 0 (Success)
+*/
+
+void print_square(int size)
+{
+	int num, times;
+
+	for (times = 0; times < size; times++)
+	{
+		for (num = 0; num < size; num++)
+		{
+			_putchar(35);
+		}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
