22c01a66c1ecb3dc20e3c4a71b61f62703c14ce5
SafyaAbouelfadel <safia.abf@gmail.com>
2024-06-27 20:40:34 -0700

    prints an integer

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,13 +14,13 @@ int main(void)
 	long num = 612852475143;
 	long max_prime = -1;
 	long i;
-	// Check for the smallest prime factor (2)
+	/* Check for the smallest prime factor (2)*/
 	while (num % 2 == 0)
 	{
 	max_prime = 2;
 	num /= 2;
 	}
-	// Check for odd factors from 3 upwards
+	/*  Check for odd factors from 3 upwards */
 	for (i = 3; i <= sqrt(num); i += 2)
 	{
 		while (num % i == 0)
@@ -29,7 +29,7 @@ int main(void)
 			num /= i;
 		}
 	}
-	// If num is still greater than 2, then num is a prime number
+	/* If num is still greater than 2, then num is a prime number */
 	if (num > 2)
 		max_prime = num;
 		printf("%ld\n", max_prime);
