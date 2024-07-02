f824b4511e8e51cef81223f7cf4c579b27c4fcd0
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-01 18:26:16 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 		{
 			sign *= -1;
 		}
-		else if (s[i] >= '0' && s[i] >= '9')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
 			result = result * 10 + (s[i] - '0');
