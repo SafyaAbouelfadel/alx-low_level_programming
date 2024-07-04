859932e5fcd7550e38549c64d15e67e71faa67d3
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-04 13:48:01 -0700

    encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encodes a string in 1337
+ *
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+*/
+
+char *leet(char *s)
+{
+	int i;
+
+	char *key = "aAeEoOtTlL";
+	char *value = "4433007711";
+	while (*s != '\0')
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if ( *s == key[i])
+			{
+				*s = value + 48;
+			}
+		}
+	s++;
+	}
+}
