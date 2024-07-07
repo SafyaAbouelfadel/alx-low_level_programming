2d10c0accc6621697bd325051619406dc77650c5
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-06 19:14:38 -0700

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -20,5 +21,5 @@ char *_strchr(char *s, char c)
 	}
 	if (*s == c)
 		return (s);
-	return ("NULL");
+	return (NULL);
 }
