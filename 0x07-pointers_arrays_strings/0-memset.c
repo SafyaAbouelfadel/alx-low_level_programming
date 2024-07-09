2d808b01098e64a95b109ad2e4edf6332f8418f3
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-08 19:26:11 -0700

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,15 +12,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i;
 	char *mems = s;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 5; j++)
-		{
-			s[i] = b;
-		}
+		s[i] = b;
 	}
 	return (mems);
 }
