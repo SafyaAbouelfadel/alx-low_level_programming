ef4bb26a444e8e11457253d6164e086e767a3163
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-10 14:26:27 -0700

    prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main_h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: string to print
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(s);
+	_puts_recursion(s++);
+}
