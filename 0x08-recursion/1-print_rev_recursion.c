8e97e15a0b326eaf557c31543ca1f271a98a67fb
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-10 14:51:22 -0700

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/* print_rev_recursion - prints a string in reverse
+/**
+ * _print_rev_recursion - prints a string in reverse
  *
  * @s: string to print
 */
- 
+
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
