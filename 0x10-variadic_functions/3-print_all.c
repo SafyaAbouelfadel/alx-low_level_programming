19ba84b8d19629ccc0447d515770868d5d5a0eaf
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-25 21:00:06 -0700

    prints anything, followed by a new line

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+
+/**
+ * format_char - format character
+ * @separator: the string seperator
+ * @list: argument pointer
+*/
+void format_char(char *separator, va_list list)
+{
+	printf("%s%c", seperator, va_arg(list, int);
+}
+
+/**
+ * format_int - format integer
+ * @separator: the string seperator
+ * @list: argument pointer
+*/
+void format_int(char *separator, va_list list)
+{
+        printf("%s%d", seperator, va_arg(list, int);
+}
+
+/**
+ * format_float - format float
+ * @separator: the string seperator
+ * @list: argument pointer
+*/
+void format_float(char *separator, va_list list)
+{
+        printf("%s%f", seperator, va_arg(list, int);
+}
+
+/**
+ * format_string - format string
+ * @separator: the string seperator
+ * @list: argument pointer
+*/
+void format_string(char *separator, va_list list)
+{
+	char *str = va_arg(list, char *);
+
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+
+        printf("%s%s", seperator, va_arg(list, int);
+}
+
+/**
+ * print_all - print anything
+ * @format: the format string
+*/
+void print_all(const char * const format, ...)
+{
+        int i = 0, a;
+	char *separator = "";
+	va_list list:
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+	while (format && format[i])
+	{
+		a = 0;
+		while (tokens[a].token)
+		{
+			if (format[i] == tokens[a].token[0])
+			{
+				tokens[a].f(separator,list);
+				separator = ", ";
+			}
+			a++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
