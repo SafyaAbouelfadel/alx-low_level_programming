792166f5334c9ff1f2f905296429fd93441d40c6
SafyaAbouelfadel <safia.abf@gmail.com>
2024-08-01 17:57:45 -0700

    prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "list.h"
+
+/**
+ * str_len - return the length
+ * @c: the string to check
+ *
+ * Return: length of string
+ */
+int str_len(char *c)
+{
+	int len = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		len++;
+	return (len);
+}
+
+/**
+ * print_list - prints a linked lists
+ * @h: pointer to the first node
+ *
+ * Return: the size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", str_len(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
