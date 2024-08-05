524e7a7a59a51333f7ecfb5abc5c2a13a9b43774
SafyaAbouelfadel <safia.abf@gmail.com>
2024-08-05 11:26:35 -0700

    fix errors

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,10 +15,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!head || !new_head)
 		return (NULL);
 
-	new_head->next = NULL; 
+	new_head->next = NULL;
 	new_head->n = n;
 	if (*head)
-		new_node->next = *head;
+		new_head->next = *head;
 	*head = new_head;
 	return (new_head);
 }
