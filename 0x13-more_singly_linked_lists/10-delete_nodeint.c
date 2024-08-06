a87ad7b698309d8d8f9a7d2ef38081b825b651ca
SafyaAbouelfadel <safia.abf@gmail.com>
2024-08-06 13:56:14 -0700

    delete a node at a given position.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t node, *previous_node;
+	listint_t *node, *previous_node;
 	unsigned int idx = 0;
 
 	if (!head || !(*head))
