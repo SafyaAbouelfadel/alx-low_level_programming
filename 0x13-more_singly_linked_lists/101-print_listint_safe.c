3755272cff2b653be0a7fa571e169bcc440e28e6
SafyaAbouelfadel <safia.abf@gmail.com>
2024-08-06 14:11:02 -0700

    prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * _reall - reallocates memory for an array of pointers
+ * _r - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
@@ -11,7 +11,7 @@
  *
  * Return: pointer to the new list
  */
-const listint_t **_reall(const listint_t **list, size_t size, const listint_t *new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 	const listint_t **newlist;
 	size_t i;
@@ -52,7 +52,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 		}
 		number++;
-		list = _reall(list, number, head);
+		list = _r(list, number, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
