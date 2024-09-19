6a2fdff8114ea148eba53c84191d3668a719b68d
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-19 16:53:19 -0700

    returns the nth node of a dlistint_t linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,15 +5,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *node = NULL;
-	unsigned int i;
+	unsigned int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	node = head;
-	for (i = 0; i > index; i++)
-		node = node->next;
-
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
 	return (NULL);
 }
