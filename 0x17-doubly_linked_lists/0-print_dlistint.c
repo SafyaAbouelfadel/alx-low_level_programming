0eb386916fd38ed5e8c0575d540963901ffabae2
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-19 13:41:25 -0700

    prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a doubly linked list
+ *
+ * @h: head of the doubly linked list
+ *
+ * Return: Always 0.
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h == 0)
+		return (0);
+
+	for (k = 0; h != 0; k++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (k);
+}
