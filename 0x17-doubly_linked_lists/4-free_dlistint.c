37490d53dcb275b4de853c159e76db1a56baf665
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-19 16:26:18 -0700

    frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - freeas a list
+ *
+ * @haed: address of the head
+ *
+ * Return: void
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while(head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
