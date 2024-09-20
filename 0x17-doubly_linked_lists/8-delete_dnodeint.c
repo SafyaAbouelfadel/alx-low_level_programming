b32e4e37d5e2dce51128c963eceb06e5de4646a2
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-19 18:36:48 -0700

    delete a node at a given position

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,7 +17,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	for (; idx != 0; idx--)
+	for (; index != 0; index--)
 	{
 		if (temp == NULL)
 			return (-1);
