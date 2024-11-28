bc74033c84d35bb5e6c52594c03e2d1f0201e5ab
SafyaAbouelfadel <safia.abf@gmail.com>
2024-11-27 17:18:54 -0800

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island represented by a 2D grid.
+
+    Args:
+        grid (List[List[int]]): A 2D grid representing the island,
+            where 1 denotes land and 0 denotes water.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edge = 0
+    size = 0
+
+    for k in range(height):
+        for j in range(width):
+            if grid[k][j] == 1:
+                size += 1
+                if (j > 0 and grid[k][j - 1] == 1):
+                    edge += 1
+                if (k  > 0 and grid[k - 1][j] == 1):
+                    edge += 1
+    return size * 4 - edge * 2
