4133058aee757ef20efaa19cf471e37112e26646
SafyaAbouelfadel <safia.abf@gmail.com>
2025-01-28 22:13:30 -0800

    C search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array || !value)
+	if (!array || size == 0)
 		return (-1);
 
-	while ( i < size)
+	while (i < size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i])
-		if ( array[i] == value)
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 		i++;
 	}
